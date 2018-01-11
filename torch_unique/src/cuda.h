3439075d9cd2ecb6733e0fed306c9139f2149aea
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-11 12:25:32 +0100

    typo

--- torch_unique/src/cuda.h
+++ torch_unique/src/cuda.h
@@ -6,10 +6,10 @@ void unique_single_cuda_Short (THCudaShortTensor*  input);
 void unique_single_cuda_Int   (THCudaIntTensor*    input);
 void unique_single_cuda_Long  (THCudaLongTensor*   input);
 
-void unique_byKey_cuda_Float (TTHCudaTensor*       key, THCudaTensor*       value);
-void unique_byKey_cuda_Double(TTHCudaDoubleTensor* key, THCudaDoubleTensor* value);
-void unique_byKey_cuda_Byte  (TTHCudaByteTensor*   key, THCudaByteTensor*   value);
-void unique_byKey_cuda_Char  (TTHCudaCharTensor*   key, THCudaCharTensor*   value);
-void unique_byKey_cuda_Short (TTHCudaShortTensor*  key, THCudaShortTensor*  value);
-void unique_byKey_cuda_Int   (TTHCudaIntTensor*    key, THCudaIntTensor*    value);
-void unique_byKey_cuda_Long  (TTHCudaLongTensor*   key, THCudaLongTensor*   value);
+void unique_byKey_cuda_Float (THCudaTensor*       key, THCudaTensor*       value);
+void unique_byKey_cuda_Double(THCudaDoubleTensor* key, THCudaDoubleTensor* value);
+void unique_byKey_cuda_Byte  (THCudaByteTensor*   key, THCudaByteTensor*   value);
+void unique_byKey_cuda_Char  (THCudaCharTensor*   key, THCudaCharTensor*   value);
+void unique_byKey_cuda_Short (THCudaShortTensor*  key, THCudaShortTensor*  value);
+void unique_byKey_cuda_Int   (THCudaIntTensor*    key, THCudaIntTensor*    value);
+void unique_byKey_cuda_Long  (THCudaLongTensor*   key, THCudaLongTensor*   value);
