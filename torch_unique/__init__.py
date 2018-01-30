cce0f67876e44cd8d7644de65fd6f84b53e5f7ae
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-30 12:44:26 +0100

    version up

--- torch_unique/__init__.py
+++ torch_unique/__init__.py
@@ -1,6 +1,6 @@
 from .functions.unique import unique
 from .functions.unique_by_key import unique_by_key
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 __all__ = ['unique', 'unique_by_key', '__version__']
