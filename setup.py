058570ee09d422bb7466d47481072db3d87941db
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-22 14:43:38 +0200

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import torch
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 url = 'https://github.com/rusty1s/pytorch_unique'
 
 install_requires = ['numpy']
--- torch_unique/__init__.py
+++ torch_unique/__init__.py
@@ -1,5 +1,5 @@
 from .unique import unique
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 __all__ = ['unique', '__version__']
